3a465bc70b55d19aa90247a37df8af52586bccf8
Edgar Simo-Serra <ess@waseda.jp>
2023-07-13 14:22:36 +0900

    Fixed update script.

--- update.sh
+++ update.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-set -x
-
 REPO="https://raw.githubusercontent.com/naev/naev/main/dat/"
 
 function dodiff () {
    basefile=$(basename $1)
-   difffile="`pwd`$2"
+   difffile="`pwd`/$2"
+   outfile="`pwd`/$1"
    tempdir=`mktemp -d`
 
    pushd "$tempdir"
-   echo wget "${repo}${$}" -o "$basefile"
-   echo patch "$basefile" "$difffile"
-   echo cp "$basefile" "$1"
+   wget "${REPO}$1" -O "$basefile"
+   patch "$basefile" < "$difffile"
+   mv "$basefile" "$outfile"
    popd
+   rmdir "$tempdir"
 }
 
 dodiff "scripts/love_shaders.lua" "love_shaders.lua.diff"
